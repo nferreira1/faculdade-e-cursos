2a8e32a1a403fda383946523da9bee81c1f5a816
nferreira1 <nathan.1402@hotmail.com>
2023-04-03 16:39:06 -0300

    Iniciando as aulas de React

---
+++ OneBitCode/React/aula09/src/components/LoremIpsum.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function LoremIpsum() {
+	return <p>Lorem ipsum dolor sit amet, consectetur adipis</p>
+}
+
+export default LoremIpsum
---
+++ OneBitCode/React/aula09/src/components/planets/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+function Planets() {
+	return (
+		<Fragment>
+			<h3>
+				Planet List
+			</h3>
+			<p>aaa</p>
+		</Fragment>
+	)
+}
+
+export default Planets
