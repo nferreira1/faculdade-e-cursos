5695eac46186f2010d453c4262836ef3c8310e89
nferreira1 <nathan.1402@hotmail.com>
2023-05-05 13:25:44 -0300

    Seção 01 e 02 concluída do curso de React do Matheus Battisti

---
+++ Matheus Battisti/React/Seção 02 - Fundamentos do React/fundamentos/src/App.js
@@ -0,0 +1,24 @@
+// styles
+import './App.css';
+
+// components
+// import FirstComponent from './components/FirstComponent'
+// import MyComponent from './components/MyComponent'
+// import TemplateExpressions from './components/TemplateExpressions '
+// import Events from './components/Events'
+import Challenge from './components/Challenge'
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Fundamentos React</h1>
+			{/* <FirstComponent />
+			<TemplateExpressions />
+			<MyComponent />
+			<Events /> */}
+			<Challenge />
+		</div>
+	)
+}
+
+export default App;
