75dbb3cbbc45cce10574fd6eb39d885dd9d94e0e
nferreira1 <nathan.1402@hotmail.com>
2023-05-05 16:55:16 -0300

    Finalizando Seção 03

--- Matheus Battisti/React/Seção 03 - Avançando no React/avancando/src/App.js
+++ Matheus Battisti/React/Seção 03 - Avançando no React/avancando/src/App.js
@@ -1,10 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 import City from './assets/city.jpg'
+import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
+import CarDetails from './components/CarDetails';
+import Container from './components/Container';
+import ExecuteFunction from './components/ExecuteFunction';
 
 function App() {
+
+	// const name = 'Nathan'
+	const [userName] = useState('Maria')
+
+	const cars = [
+		{ id: 1, brand: 'Ferrari', color: 'Amarela', newCar: true, km: 0 },
+		{ id: 2, brand: 'KIA', color: 'Branco', newCar: false, km: 343434 },
+		{ id: 3, brand: 'renault', color: 'azul', newCar: false, km: 234 },
+	]
+
+	function showMessage() {
+		console.log('Evento do componente pai!')
+	}
+
 	return (
 		<div className="App">
 			<h1>Avançando no React</h1>
@@ -22,6 +42,32 @@ function App() {
 			<ManageData />
 
 			<ListRender />
+
+			<ConditionalRender />
+
+			{/* props */}
+			<ShowUserName name={userName} />
+
+			{/* destructuring*/}
+			<CarDetails brand='chevrolet' km={100000} color='AZUL' newCar={false} />
+
+			{/* reaproveitando */}
+			<CarDetails brand='ford' color='rosa' km={100000} newCar={false} />
+			<CarDetails brand='fiat' color='branco' km={0} newCar={true} />
+			<CarDetails brand='fiat' color='branco' km={0} newCar={true} />
+
+			{/* loop em array de obejtos */}
+			{cars.map((car, i) => (
+				<CarDetails key={i} brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
+			))}
+
+			{/* cildren */}
+			<Container myValue='testing'>
+				<p>E este é o conteúdo</p>
+			</Container>
+
+			{/* executar função */}
+			<ExecuteFunction myFunction={showMessage} />
 		</div>
 	)
 }
--- Matheus Battisti/React/Seção 03 - Avançando no React/avancando/src/components/ManageData.js
+++ Matheus Battisti/React/Seção 03 - Avançando no React/avancando/src/components/ManageData.js
@@ -5,7 +5,7 @@ const ManageData = () => {
 	let someData = 10
 
 	// useState serve para re-renderizar a página, inicialmente é passado um valor para o useState e quando o botão é clicado, ele re-renderiza a página para exibir o valor
-	const [number, setNumber] = useState(15)
+	const [number, setNumber] = useState(10)
 
 	return (
 		<div>
@@ -15,7 +15,7 @@ const ManageData = () => {
 			</div>
 			<div>
 				<p>Valor: {number}</p>
-				<button onClick={() => setNumber(25)}>Mudar o state</button>
+				<button onClick={() => setNumber(15)}>Mudar o state</button>
 			</div>
 		</div>
 	)
